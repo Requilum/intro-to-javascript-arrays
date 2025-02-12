75cf69c879d6dfd2cdc837d3433b6f0c8c72d9d0
aalma <aalma@example.com>
2025-02-12 20:30:51 +0300

    finished array lecture

--- app.js
+++ app.js
@@ -61,3 +61,48 @@ for (let movie of movies) {
 
 console.log("");
 /*--------------- forEach() ------------------*/
+const newMovies = ["Barbie", "Arrival", "Get Out", "Coco"];
+
+movies.forEach((newMovies) => {
+  console.log(newMovies);
+});
+
+// Barbie
+// Arrival
+// Get Out
+// Coco
+
+//In addition to the current element, forEach() also provides the index of the current element to the callback function:
+movies.forEach((newMovies, index) => {
+  if (index === 2) console.log("This is the third movie:");
+  console.log(newMovies);
+});
+
+//   Which will result in the following output:
+
+// Barbie
+// Arrival
+// This is the third movie:
+// Get Out
+// Coco
+
+console.log("");
+/*--------------- join() ------------------*/
+// as a reminder, movies is ['Barbie', 'Arrival', 'Get Out', 'Coco']
+let movieString = newMovies.join();
+// movieString is 'Barbie,Arrival,Get Out,Coco'
+
+movieString = newMovies.join(" -- ");
+// movieString is 'Barbie -- Arrival -- Get Out -- Coco'
+
+/*--------------- You Do----------------*/
+const books = ["The shining", "Pride and Prejudice"];
+console.log(books[2 - 1]); // Pride and Prejudice
+// Function used in this mannaer to simplify it with common sense. Since the array starts with an index of 0, we subtract 1 from the index to get the correct value.
+
+movies[2 - 1] = "Dune";
+console.log(movies[1]); // Dune
+
+for (let idx = 0; idx < books.length; idx++) {
+  console.log(books[idx]);
+}
